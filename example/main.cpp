4fe66290c0faa35b5644b4dc07c62c2bbb995c78
Spartak Vasilev <spartak.lyubomirov.vasilev@gmail.com>
2015-06-02 11:49:41 +0300

    in the new repo

---
+++ example/main.cpp
@@ -0,0 +1,159 @@
+#include <iostream>
+#include <cassert>
+#include <cstring>
+
+using namespace std;
+
+class People {
+private:
+    char*name;
+    int age;
+    void init(char*n, int a) {
+        delete[]this -> name;
+        this -> name = new char[strlen(n) + 1];
+        assert(name);
+        strcpy(this -> name, n);
+
+        this -> age = a;
+    }
+
+public:
+    People(char*n = "Ivan", int a = 20) {
+        cout << "constr. People\n";
+        this -> name = NULL;
+        init(n, a);
+    }
+
+    ~People() {
+        cout << "destr.People\n";
+        delete[]this -> name;
+    }
+
+    People(const People &other) {
+        cout << "copy constr. People\n";
+        init(other.name, other.age);
+    }
+
+    People &operator=(const People &other) {
+        cout << "operator= People\n";
+        if(this != &other) {
+            init(other.name, other.age);
+        }
+        return *this;
+    }
+
+    virtual void printPeople() {
+        cout << this -> name << ' ' << this -> age << endl;
+    }
+
+};
+
+class Mother : virtual public People {
+private:
+    char*eyeColour;
+
+public:
+    void setEyeColour(char*eye) {
+        delete[]this -> eyeColour;
+        this -> eyeColour = new char[strlen(eye) + 1];
+        assert(eyeColour);
+        strcpy(this -> eyeColour, eye);
+    }
+
+    Mother(char*n = "Maria", int a = 30, char*e = "Blue") : People(n, a) {
+            cout << "Mother constr.\n";
+        this -> eyeColour = NULL;
+        setEyeColour(e);
+    }
+
+    ~Mother() {
+        cout << "destr. Mother\n";
+        delete[]this -> eyeColour;
+    }
+
+    Mother(const Mother &other) {
+        cout << "copy constr. Mother\n";
+        setEyeColour(other.eyeColour);
+    }
+
+    Mother &operator=(const Mother &other) {
+        if(this != &other) {
+            People::operator=(other);
+            setEyeColour(other.eyeColour);
+        }
+        return *this;
+    }
+
+    void printPeople() {
+        People::printPeople();
+        cout << this -> eyeColour << endl;
+    }
+
+};
+
+class Father : virtual public People {
+private:
+    char*surName;
+
+public:
+    void setSurName(char*s) {
+        delete[]this -> surName;
+        this -> surName = new char[strlen(s) + 1];
+        assert(surName);
+        strcpy(this -> surName, s);
+    }
+
+    Father(char*n = "Mitko", int a = 40, char*s = "Ivanov") : People(n, a) {
+        cout << "Father constr\n";
+        this -> surName = NULL;
+        setSurName(s);
+    }
+
+    ~Father() {
+        cout << "Father destr.\n";
+        delete[]this -> surName;
+    }
+
+    Father(const Father &other) {
+        cout << "Father constr.\n";
+        setSurName(other.surName);
+    }
+
+    Father &operator=(const Father &other) {
+        cout << "Father operator=\n";
+        if(this != &other) {
+            People::operator=(other);
+            setSurName(other.surName);
+        }
+        return *this;
+    }
+
+    void printPeople() {
+        People::printPeople();
+        cout << this -> surName << endl;
+    }
+
+};
+/*
+class Child : virtual public People, public Mother, public Father {
+public:
+    Child() : People(), Mother(), Father() {
+        cout << "Child constr.\n";
+    }
+
+    ~Child() {
+        cout << "Child destr.\n";
+    }
+
+    void printPeople() const {
+        printPeople();
+    }
+
+};
+*/
+
+int main()
+{
+
+    return 0;
+}
